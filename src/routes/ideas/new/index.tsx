c8c3fb7bae224e1063bf16cad18ec728e1e04565
laurentvuillaume <figura.graphik@gmail.com>
2025-11-28 04:37:50 +0100

    fix UI newId page

--- src/routes/ideas/new/index.tsx
+++ src/routes/ideas/new/index.tsx
@@ -95,7 +95,7 @@ function NewIdeaPage() {
             htmlFor="description"
             className="block text-gray-700 font-medium mb-1"
           >
-            Tile
+            Description
           </label>
           <textarea
             id="description"

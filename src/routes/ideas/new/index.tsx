13a4442139acadc53ed893f7c4f39251e640fc85
laurentvuillaume <figura.graphik@gmail.com>
2025-11-28 04:50:38 +0100

    fix UI newId page

--- src/routes/ideas/new/index.tsx
+++ src/routes/ideas/new/index.tsx
@@ -59,7 +59,7 @@ function NewIdeaPage() {
             htmlFor="title"
             className="block text-gray-700 font-medium mb-1"
           >
-            Tile
+            Title
           </label>
           <input
             id="title"
